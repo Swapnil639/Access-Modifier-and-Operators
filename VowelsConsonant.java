22388dec1be1de213363ec453e47de12904fb1d4
Swapnil639 <swapnilpatil631997@gmail.com>
2023-03-30 16:41:55 +0530

    Added :Check vowels and Consonant

---
+++ VowelsConsonant.java
@@ -0,0 +1,44 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class VowelsConsonant {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String a=scanner.next();
+        switch (a){
+            case "a":
+                System.out.println("a is vowel");
+                break;
+            case "e":
+                System.out.println("e is vowel");
+                break;
+            case "i":
+                System.out.println("i is vowel");
+                break;
+            case "o":
+                System.out.println("o is vowel");
+                break;
+            case "u":
+                System.out.println("u is vowel");
+                break;
+            case "A":
+                System.out.println("A is vowel");
+                break;
+            case "E":
+                System.out.println("E is vowel");
+                break;
+            case "I":
+                System.out.println("I is vowel");
+                break;
+            case "O":
+                System.out.println("O is vowel");
+                break;
+            case "U":
+                System.out.println("U is vowel");
+                break;
+            default:
+                System.out.println(a+" is consonant");
+        }
+    }
+}
