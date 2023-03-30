b90d0f11907556bcfb3e7e3f3a93bbbd10bc4a21
Swapnil639 <swapnilpatil631997@gmail.com>
2023-03-30 16:29:24 +0530

    sum of n natural number

---
+++ NaturalNum.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class NaturalNum {
+    public static void main(String[] args) {
+        int input=5,sum=0;
+        while (input>0){
+            sum=sum+input;
+            input--;
+        }
+        System.out.println(sum);
+    }
+}
